388024d1975bc64b12098a8086b46eb122b1f712
joseMarciano <marcianojosepaulo@gmail.com>
2022-01-23 20:35:25 -0300

    feat: add server instance

---
+++ src/main/server.ts
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+import { MongoHelper } from '../infra/db/mongo/mongoHelper'
+import env from './config/env'
+import { app } from './config/app'
+
+MongoHelper.connect(env.mongoUrl)
+  .then(startServer)
+  .catch(console.error)
+
+function startServer (): void {
+  app.listen(env.port, () => console.log(`Server is running at ${env.port}`))
+}
