0691a5ca523898d352b5c93f6ed245ea1d078608
joseMarciano <marcianojosepaulo@gmail.com>
2022-01-23 19:05:47 -0300

    refactor: change structure of error responses

--- src/presentation/helper/httpHelper.ts
+++ src/presentation/helper/httpHelper.ts
@@ -3,14 +3,14 @@ import { HttpResponse } from '../protocols'
 export const badRequest = (error: Error): HttpResponse => {
   return {
     statusCode: 400,
-    body: error.name
+    body: getDefaultErrorStructure(error)
   }
 }
 
 export const serverError = (error: Error): HttpResponse => {
   return {
     statusCode: 500,
-    body: error.name
+    body: getDefaultErrorStructure(error)
   }
 }
 
@@ -18,5 +18,12 @@ export const ok = (body: any): HttpResponse => {
   return {
     statusCode: 200,
     body
+  }
+}
+
+function getDefaultErrorStructure (error: Error): any {
+  return {
+    message: error.message,
+    error: error.name
   }
 }
