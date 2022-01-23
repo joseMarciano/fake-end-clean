1dbe1b4f0172177b9c59179f9cb8785ef66b089f
joseMarciano <marcianojosepaulo@gmail.com>
2022-01-23 15:17:21 -0300

    test: ensure disconnect mongoDb when MongoHelper calls disconnect

--- src/infra/db/mongo/mongoHelper.spec.ts
+++ src/infra/db/mongo/mongoHelper.spec.ts
@@ -1,13 +1,26 @@
 import { MongoHelper } from './mongoHelper'
 
+const MONGO_URL = 'mongodb://localhost:27017/fake-end'
+
 describe('mongoHelper', () => {
   test('Should connect when connect is called', async () => {
     let collection = MongoHelper.getCollection('any')
     expect(collection).toBeFalsy()
 
-    await MongoHelper.connect('mongodb://localhost:27017/fake-end')
+    await MongoHelper.connect(MONGO_URL)
 
     collection = MongoHelper.getCollection('any')
     expect(collection).toBeTruthy()
+  })
+  test('Should disconnect when disconnect is called', async () => {
+    await MongoHelper.connect(MONGO_URL)
+
+    let collection = MongoHelper.getCollection('any')
+    expect(collection).toBeTruthy()
+
+    await MongoHelper.disconnect()
+
+    collection = MongoHelper.getCollection('any')
+    expect(collection).toBeFalsy()
   })
 })
