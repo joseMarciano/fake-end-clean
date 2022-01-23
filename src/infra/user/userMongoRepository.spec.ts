458bdca2b6378bfcd5ef8f0c5174afd5b9ff73fd
joseMarciano <marcianojosepaulo@gmail.com>
2022-01-23 16:23:42 -0300

    test: ensure UserMongoRepository returns an User on success

---
+++ src/infra/user/userMongoRepository.spec.ts
@@ -0,0 +1,46 @@
+import { UserModel } from 'src/domain/usecases/AddUser'
+import { MongoHelper } from '../db/mongo/mongoHelper'
+import { UserMongoRespository } from './UserMongoRepository'
+
+const makeFakeUserModel = (): UserModel => ({
+  email: 'any_email@mail.com',
+  name: 'any_name',
+  password: 'any_password'
+})
+interface SutTypes {
+  sut: UserMongoRespository
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new UserMongoRespository()
+
+  return {
+    sut
+  }
+}
+
+describe('UserMongoRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const userCollection = await MongoHelper.getCollection('users')
+    await userCollection.deleteMany({})
+  })
+
+  test('Should return an User on add success', async () => {
+    const { sut } = makeSut()
+
+    const user = await sut.add(makeFakeUserModel())
+
+    expect(user).toBeTruthy()
+    expect(user.id).toBeTruthy()
+    expect(user.name).toBe('any_name')
+    expect(user.password).toBe('any_password')
+  })
+})
