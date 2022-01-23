40fe0797d8539149e1961559f106f6a9b03f07b1
joseMarciano <marcianojosepaulo@gmail.com>
2022-01-23 19:41:39 -0300

    chore: add port to env file

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,4 @@
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/fake-end'
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/fake-end',
+  port: process.env.PORT ?? 8080
 }
