6eab4170bab1552490155772a5e44281777f78e3
joseMarciano <marcianojosepaulo@gmail.com>
2022-01-29 19:45:54 -0300

    fix: remove github auth for a while

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/fake-end',
   port: process.env.PORT ?? 8080,
-  defaultPath: process.env.DEFAULT_PATH ?? '/api',
-  signUpUrlGitHub: `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID ?? '20cbf116d9a6a6412589'}`
+  defaultPath: process.env.DEFAULT_PATH ?? '/api'
 }
