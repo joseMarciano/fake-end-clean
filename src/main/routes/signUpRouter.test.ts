8f4c51e5a5b2cd866f409db524ec181b7de10dcd
joseMarciano <marcianojosepaulo@gmail.com>
2022-01-29 09:56:41 -0300

    test: ensure signupRouter returns an user on signup success

---
+++ src/main/routes/signUpRouter.test.ts
@@ -0,0 +1,38 @@
+import { app } from '../config/app'
+import request from 'supertest'
+import env from '../config/env'
+import { MongoHelper } from '../../infra/db/mongo/mongoHelper'
+
+describe('signUpRouter', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(env.mongoUrl)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const userCollection = await MongoHelper.getCollection('users')
+    await userCollection.deleteMany({})
+  })
+
+  describe('POST /signup', () => {
+    test('Should return an User on signup success', async () => {
+      const response = await request(app)
+        .post(`${env.defaultPath}/signup`)
+        .send({
+          email: 'marcianojosepaulo@email.com',
+          name: 'marcianojosepaulo',
+          password: '123'
+        })
+
+      expect(response.status).toBe(200)
+      expect(response.body).toBeTruthy()
+      expect(response.body.id).toBeTruthy()
+      expect(response.body.email).toBe('marcianojosepaulo@email.com')
+      expect(response.body.name).toBe('marcianojosepaulo')
+      expect(response.body.password).toBe('123')
+    })
+  })
+})
