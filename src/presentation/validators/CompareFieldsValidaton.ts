1dbc46182cf568589e53c8cddf6e36b9b5aba3d0
joseMarciano <marcianojosepaulo@gmail.com>
2022-01-30 18:23:54 -0300

    feat: create a CompareFields Validation

---
+++ src/presentation/validators/CompareFieldsValidaton.ts
@@ -0,0 +1,51 @@
+import { InvalidParamError } from '../controllers/errors/InvalidParamError'
+import { Validator } from '../protocols'
+
+export class CompareFieldsValidation implements Validator {
+  private fieldName: string
+  private fieldNameToCompare: string
+
+  validate (input: any): Error | null {
+    return input[this.fieldName] === input[this.fieldNameToCompare] ? null : new InvalidParamError(this.fieldNameToCompare)
+  }
+
+  static builder (): Builder {
+    return new Builder(new CompareFieldsValidation())
+  }
+
+  set field (fieldName: string) {
+    this.fieldName = fieldName
+  }
+
+  get field (): string {
+    return this.fieldName
+  }
+
+  set fieldToCompare (fieldNameToCompare: string) {
+    this.fieldNameToCompare = fieldNameToCompare
+  }
+
+  get fieldToCompare (): string {
+    return this.fieldNameToCompare
+  }
+}
+
+class Builder {
+  constructor (
+    private readonly compareFieldsValidation: CompareFieldsValidation
+  ) { }
+
+  field (fieldName: string): Builder {
+    this.compareFieldsValidation.field = fieldName
+    return this
+  }
+
+  fieldToCompare (fieldNameToCompare: string): Builder {
+    this.compareFieldsValidation.fieldToCompare = fieldNameToCompare
+    return this
+  }
+
+  build (): CompareFieldsValidation {
+    return this.compareFieldsValidation
+  }
+}
