dd7b125e55a6a7c55ec82776c8131529f9d8a45f
joseMarciano <marcianojosepaulo@gmail.com>
2022-01-30 18:38:29 -0300

    feat: create a RequiredFieldValidation

---
+++ src/presentation/validators/RequiredFieldValidation.ts
@@ -0,0 +1,37 @@
+import { MissingParamError } from '../controllers/errors/MissingParamError'
+import { Validator } from '../protocols'
+
+export class RequiredFieldValidation implements Validator {
+  private fieldName: string
+
+  validate (input: any): Error | null {
+    return input[this.fieldName] ? null : new MissingParamError(this.fieldName)
+  }
+
+  static builder (): Builder {
+    return new Builder(new RequiredFieldValidation())
+  }
+
+  set field (fieldName: string) {
+    this.fieldName = fieldName
+  }
+
+  get field (): string {
+    return this.fieldName
+  }
+}
+
+class Builder {
+  constructor (
+    private readonly requiredFieldValidation: RequiredFieldValidation
+  ) {}
+
+  field (fieldName: string): Builder {
+    this.requiredFieldValidation.field = fieldName
+    return this
+  }
+
+  build (): RequiredFieldValidation {
+    return this.requiredFieldValidation
+  }
+}
