a2f3d2cbbb600999ee74aa305646b07c176b4916
joseMarciano <marcianojosepaulo@gmail.com>
2022-01-30 18:26:29 -0300

    test: ensure CompareFieldsValidation returns an InvalidParamError if fields not match

--- src/presentation/validators/compareFieldsValidation.spec.ts
+++ src/presentation/validators/compareFieldsValidation.spec.ts
@@ -1,3 +1,4 @@
+import { InvalidParamError } from '../controllers/errors/InvalidParamError'
 import { CompareFieldsValidation } from './CompareFieldsValidaton'
 
 describe('CompareFieldsValition', () => {
@@ -9,5 +10,19 @@ describe('CompareFieldsValition', () => {
 
     expect(sut.field).toBe('password')
     expect(sut.fieldToCompare).toBe('passwordConfirmation')
+  })
+
+  test('Should return InvalidParamError if fields not match', () => {
+    const sut = CompareFieldsValidation.builder()
+      .field('password')
+      .fieldToCompare('passwordConfirmation')
+      .build()
+
+    const error = sut.validate({
+      password: 'any_password',
+      passwordConfirmation: 'different_password'
+    })
+
+    expect(error).toEqual(new InvalidParamError('passwordConfirmation'))
   })
 })
