dc49353f6f8869a12e5958546368e85f64f732f8
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-07 20:24:23 -0300

    test: ensure EmailValidatorAdapter call isemail with correct value

---
+++ src/infra/validators/emailValidatorAdapter.spec.ts
@@ -0,0 +1,30 @@
+import IsEmail from 'isemail'
+import { EmailValidatorAdapter } from './EmailValidatorAdapter'
+
+jest.mock('isemail', () => ({
+  validate (_input: string): boolean {
+    return true
+  }
+}))
+
+interface SutTypes {
+  sut: EmailValidatorAdapter
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new EmailValidatorAdapter()
+  return {
+    sut
+  }
+}
+
+describe('EmailValidadorAdapter', () => {
+  test('Should call IsEmail with correct values', () => {
+    const { sut } = makeSut()
+
+    const validateSpy = jest.spyOn(IsEmail, 'validate')
+    sut.validate('any_input')
+
+    expect(validateSpy).toHaveBeenCalledWith('any_input')
+  })
+})
