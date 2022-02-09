6994bd98de213abe13be8b8a56aebb4e3d16bc9c
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-08 23:43:31 -0300

    feat: add EmailNotification to a signUpControllerFactory

--- src/main/factories/controller/signup/signUpControllerFactory.ts
+++ src/main/factories/controller/signup/signUpControllerFactory.ts
@@ -1,3 +1,4 @@
+import { EmailNotification } from '../../../../infra/notification/EmailNotification'
 import { DbAddUser } from '../../../../data/usecases/user/DbAddUser'
 import { UserMongoRespository } from '../../../../infra/user/UserMongoRepository'
 import { SignUpController } from '../../../../presentation/controllers/signUp/SignUpController'
@@ -5,7 +6,8 @@ import { makeBcryptAdapter } from '../../cryptography/bcryptAdapterFactory'
 import { makeValidationComposite } from './signUpValidationCompositeFactory'
 
 export const makeSignUpController = (): SignUpController => {
+  const emailNotification = new EmailNotification()
   const userMongoRepository = new UserMongoRespository()
   const addUser = new DbAddUser(userMongoRepository, makeBcryptAdapter(), userMongoRepository)
-  return new SignUpController(addUser, makeValidationComposite())
+  return new SignUpController(addUser, makeValidationComposite(), emailNotification)
 }
