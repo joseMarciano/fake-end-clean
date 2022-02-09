48020dfa8993483b2808eee09fb5cc725d39ce55
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-08 23:40:10 -0300

    feat: ensure EmailNotification calls sendMail with correct values

---
+++ src/infra/notification/EmailNotification.ts
@@ -0,0 +1,29 @@
+import { Notification } from '../../data/notification/Notification'
+import nodeMailer, { Transporter } from 'nodemailer'
+
+export class EmailNotification implements Notification {
+  private readonly transporter: Transporter
+
+  constructor () {
+    this.transporter = this.setUp()
+  }
+
+  async send (input: any): Promise<void> {
+    await this.transporter.sendMail({
+      from: 'Fake end 👻',
+      to: input,
+      subject: 'Welcome to fake end ✔',
+      html: '<a target="_blank" href="http://localhost:8080/active">Click here to activate your account</a>'
+    })
+  }
+
+  private setUp (): Transporter {
+    return nodeMailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: 'fakendapi@gmail.com',
+        pass: 'corei57th'
+      }
+    })
+  }
+}
