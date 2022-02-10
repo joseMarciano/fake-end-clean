5d803d39088d2c001b8e96b2cf51921b2d04f31e
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-10 20:17:28 -0300

    refactor: get return of findById and pass to active

--- src/presentation/controllers/login/active/ActiveUserController.ts
+++ src/presentation/controllers/login/active/ActiveUserController.ts
@@ -14,16 +14,9 @@ export class ActiveUserController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { id } = await this.decrypter.decrypt(httpRequest.params?.user)
+      const user = await this.findUserById.findById(id as string)
+      await this.activateUser.active(user)
 
-      await this.findUserById.findById(id as string)
-
-      await this.activateUser.active({
-        id: 'any_id',
-        email: 'any_email',
-        isActive: false,
-        name: 'any_name',
-        password: 'any_password'
-      })
       return await Promise.resolve({ body: null, statusCode: 999 })
     } catch (error) {
       return serverError(error)
