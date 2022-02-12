dbf435410b0dc423f5709959bd4d2d0fe72dcda5
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-12 11:58:48 -0300

    test: ensure MongoHelper create a userAccessTokenCollection

--- src/infra/db/mongo/mongoHelper.spec.ts
+++ src/infra/db/mongo/mongoHelper.spec.ts
@@ -1,8 +1,21 @@
+import { CollectionInfo, Db } from 'mongodb'
 import { MongoHelper } from './mongoHelper'
 
 const MONGO_URL = process.env.MONGO_URL as string
 
+const getCollectionByName = async (db: Db, collectionName: string): Promise<CollectionInfo | undefined> => {
+  const listCollections = db.listCollections()
+  const array = await listCollections.toArray()
+  return array.find((collection: CollectionInfo) => collection.name === collectionName)
+}
+
 describe('mongoHelper', () => {
+  beforeEach(async () => {
+    await MongoHelper.connect(MONGO_URL)
+    await MongoHelper.client.db().dropDatabase()
+    await MongoHelper.disconnect()
+  })
+
   test('Should connect when connect is called', async () => {
     let client = MongoHelper.client
     expect(client).toBeFalsy()
@@ -33,5 +46,16 @@ describe('mongoHelper', () => {
 
     collection = await MongoHelper.getCollection('any')
     expect(collection).toBeTruthy()
+  })
+  test('Should create usersAccessToken collection', async () => {
+    await MongoHelper.connect(MONGO_URL)
+    const db = MongoHelper.client.db()
+
+    await MongoHelper.createCustomCollections()
+    const userAccessTokenCollectionInfo = await getCollectionByName(db, 'usersAccessToken')
+
+    expect(userAccessTokenCollectionInfo).toBeTruthy()
+
+    await MongoHelper.disconnect()
   })
 })
