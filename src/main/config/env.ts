9c4d4d1477f9a6390c795c4d1cb510f9eeee1d42
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-13 11:34:07 -0300

    chore: add JWT_SECRET_KEY environment

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,6 @@
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/fake-end',
   port: process.env.PORT ?? 8080,
-  defaultPath: process.env.DEFAULT_PATH ?? '/api'
+  defaultPath: process.env.DEFAULT_PATH ?? '/api',
+  jwtSecretKey: process.env.JWT_SECRET_KEY ?? 'secret'
 }
