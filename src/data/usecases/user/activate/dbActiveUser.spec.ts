cdab36e69d59d941e139910dd9752f8e95c61264
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-13 20:22:16 -0300

    test: ensure DbActiveUser calls FindUserByEmailRepository with correct value

--- src/data/usecases/user/activate/dbActiveUser.spec.ts
+++ src/data/usecases/user/activate/dbActiveUser.spec.ts
@@ -1,10 +1,30 @@
+import { User } from '../../../../domain/model/User'
 import { ActivateUserModel } from '../../../../domain/usecases/user/activate/ActivateUser'
 import { DbActiveUser } from './DbActiveUser'
-import { Decrypter } from 'src/data/protocols/cryptography/Decrypter'
+import { Decrypter } from '../../../../data/protocols/cryptography/Decrypter'
+import { FindUserByEmailRepository } from '../../../../data/protocols/user/FindUserByEmailRepository'
+
+const makeFakeUser = (): User => ({
+  id: 'any_id',
+  email: 'any_email',
+  isActive: false,
+  name: 'any_name',
+  password: 'any_password'
+})
 
 const makeFakeUserActivateModel = (): ActivateUserModel => ({
   encryptedValue: 'any_encrypted_value'
 })
+
+const makeFindUserByEmailRepository = (): FindUserByEmailRepository => {
+  class FindUserByEmailRepositoryStub implements FindUserByEmailRepository {
+    async findByEmail (_email: string): Promise<User> {
+      return await Promise.resolve(makeFakeUser())
+    }
+  }
+
+  return new FindUserByEmailRepositoryStub()
+}
 
 const makeDecrypter = (): Decrypter => {
   class DecrypterStub implements Decrypter {
@@ -22,13 +42,21 @@ const makeDecrypter = (): Decrypter => {
 interface SutTypes {
   sut: DbActiveUser
   decrypterStub: Decrypter
+  findUserByEmailRepositoryStub: FindUserByEmailRepository
 }
 const makeSut = (): SutTypes => {
   const decrypterStub = makeDecrypter()
-  const sut = new DbActiveUser(decrypterStub)
+  const findUserByEmailRepositoryStub = makeFindUserByEmailRepository()
+
+  const sut = new DbActiveUser(
+    decrypterStub,
+    findUserByEmailRepositoryStub
+  )
+
   return {
     sut,
-    decrypterStub
+    decrypterStub,
+    findUserByEmailRepositoryStub
   }
 }
 
@@ -40,5 +68,13 @@ describe('DbActiveUser', () => {
     await sut.active(makeFakeUserActivateModel())
 
     expect(decryptSpy).toHaveBeenCalledWith('any_encrypted_value')
+  })
+  test('Should call FindUserByEmailRepository with correct value', async () => {
+    const { sut, findUserByEmailRepositoryStub } = makeSut()
+
+    const findUserByEmailStub = jest.spyOn(findUserByEmailRepositoryStub, 'findByEmail')
+    await sut.active(makeFakeUserActivateModel())
+
+    expect(findUserByEmailStub).toHaveBeenCalledWith('any_email')
   })
 })
