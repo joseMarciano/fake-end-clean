40e17fcb16f9069a0ce33ed481cab6fd54c01dc2
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-13 11:37:54 -0300

    feat: refactor change dependencies to use factory methods

--- src/main/factories/controller/signup/signUpControllerFactory.ts
+++ src/main/factories/controller/signup/signUpControllerFactory.ts
@@ -1,13 +1,13 @@
 import { EmailNotification } from '../../../../infra/notification/EmailNotification'
-import { DbAddUser } from '../../../../data/usecases/user/add/DbAddUser'
-import { UserMongoRespository } from '../../../../infra/user/UserMongoRepository'
 import { SignUpController } from '../../../../presentation/controllers/login/signUp/SignUpController'
-import { makeBcryptAdapter } from '../../cryptography/bcryptAdapterFactory'
 import { makeValidationComposite } from './signUpValidationCompositeFactory'
+import { makeDbAuthentication } from '../../usecases/user/dbAuthenticationFactory'
+import { makeDbAddUser } from '../../usecases/user/dbAddUserFactory'
 
 export const makeSignUpController = (): SignUpController => {
+  const dbAuthentication = makeDbAuthentication()
+  const addUser = makeDbAddUser()
+  const validatonComposite = makeValidationComposite()
   const emailNotification = new EmailNotification()
-  const userMongoRepository = new UserMongoRespository()
-  const addUser = new DbAddUser(userMongoRepository, makeBcryptAdapter(), userMongoRepository)
-  return new SignUpController(addUser, makeValidationComposite(), emailNotification)
+  return new SignUpController(addUser, validatonComposite, emailNotification, dbAuthentication)
 }
