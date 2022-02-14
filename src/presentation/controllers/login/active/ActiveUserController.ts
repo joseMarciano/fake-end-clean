8a61fe4d8dc31ed3695415a79631c1e82138a87b
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-13 21:06:36 -0300

    feat: ensure ActiveUserController calls validator with correct values

--- src/presentation/controllers/login/active/ActiveUserController.ts
+++ src/presentation/controllers/login/active/ActiveUserController.ts
@@ -1,14 +1,19 @@
-import { ok, serverError } from '../../../../presentation/helper/httpHelper'
-import { Controller, HttpRequest, HttpResponse } from '../../../../presentation/protocols'
+import { badRequest, ok, serverError } from '../../../../presentation/helper/httpHelper'
+import { Controller, HttpRequest, HttpResponse, Validator } from '../../../../presentation/protocols'
 import { ActivateUser } from '../../../../domain/usecases/user/activate/ActivateUser'
 
 export class ActiveUserController implements Controller {
   constructor (
-    private readonly activateUser: ActivateUser
+    private readonly activateUser: ActivateUser,
+    private readonly validator: Validator
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const error = this.validator.validate(httpRequest)
+
+      if (error) return badRequest(error)
+
       const user = await this.activateUser.active({ encryptedValue: httpRequest.params.user })
 
       return ok(user)
