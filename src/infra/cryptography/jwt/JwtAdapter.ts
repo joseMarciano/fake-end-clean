0fe7f4450ac35edf2e253dc24362ebd462f27ffe
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-13 21:35:41 -0300

    feat: ensure JwtAdapter calls decode with correct values

--- src/infra/cryptography/jwt/JwtAdapter.ts
+++ src/infra/cryptography/jwt/JwtAdapter.ts
@@ -1,12 +1,17 @@
 import jwt from 'jsonwebtoken'
+import { Decrypter } from '../../../data/protocols/cryptography/Decrypter'
 import { Encrypter } from '../../../data/protocols/cryptography/Encrypter'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor (
     private readonly secretKey: string
   ) {}
 
   async encrypt (input: any): Promise<string> {
     return await Promise.resolve(jwt.sign(input, this.secretKey))
+  }
+
+  async decrypt (input: string): Promise<any> {
+    return await Promise.resolve(jwt.decode(input))
   }
 }
