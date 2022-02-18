dc5dd96f682eec0a6ab1747eb221d632e24e37f3
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-17 21:24:17 -0300

    test: ensure AddProjectController calls AddProject usecase with correct values

---
+++ src/presentation/controllers/project/add/addProjectController.spec.ts
@@ -0,0 +1,61 @@
+import { AddProject, AddProjectModel } from 'src/domain/usecases/project/add/AddProject'
+import { Project } from '../../../../domain/model/Project'
+import { HttpRequest } from '../../../../presentation/protocols'
+import { AddProjectController } from './AddProjectController'
+
+const makeFakeHttpRequest = (): HttpRequest => ({
+  body: {
+    title: 'any_title',
+    description: 'any_description'
+  },
+  params: {
+    userId: 'any_id'
+  }
+})
+
+const makeFakeProject = (): Project => ({
+  id: 'any_id',
+  title: 'any_title',
+  description: 'any_description',
+  secretKey: 'any_secret_key',
+  user: 'any_id'
+})
+
+const makeAddProject = (): AddProject => {
+  class AddProjectStub implements AddProject {
+    async add (userModel: AddProjectModel): Promise<Project> {
+      return makeFakeProject()
+    }
+  }
+
+  return new AddProjectStub()
+}
+
+interface SutTypes {
+  sut: AddProjectController
+  addProjectStub: AddProject
+}
+
+const makeSut = (): SutTypes => {
+  const addProjectStub = makeAddProject()
+  const sut = new AddProjectController(addProjectStub)
+
+  return {
+    sut,
+    addProjectStub
+  }
+}
+
+describe('AddProjectController', () => {
+  test('Should call AddProject with correct values ', async () => {
+    const { sut, addProjectStub } = makeSut()
+
+    const addSpy = jest.spyOn(addProjectStub, 'add')
+    await sut.handle(makeFakeHttpRequest())
+
+    expect(addSpy).toHaveBeenCalledWith({
+      ...makeFakeHttpRequest().body,
+      ...makeFakeHttpRequest().params
+    })
+  })
+})
