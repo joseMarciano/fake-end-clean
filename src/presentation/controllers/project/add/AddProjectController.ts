9f58d071278a4987a3b6928391aa7bff71729fda
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-17 22:03:36 -0300

    feat: ensure AddProjectController calls validator with correct values

--- src/presentation/controllers/project/add/AddProjectController.ts
+++ src/presentation/controllers/project/add/AddProjectController.ts
@@ -1,14 +1,18 @@
 import { ok, serverError } from '../../../../presentation/helper/httpHelper'
 import { AddProject } from '../../../../domain/usecases/project/add/AddProject'
-import { Controller, HttpRequest, HttpResponse } from '../../../../presentation/protocols'
+import { Controller, HttpRequest, HttpResponse, Validator } from '../../../../presentation/protocols'
 
 export class AddProjectController implements Controller {
   constructor (
-    private readonly addProject: AddProject
+    private readonly addProject: AddProject,
+    private readonly validator: Validator
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validator.validate(httpRequest.body)
+      this.validator.validate(httpRequest.params.userId)
+
       const userModel = {
         ...httpRequest.body,
         ...httpRequest.params
