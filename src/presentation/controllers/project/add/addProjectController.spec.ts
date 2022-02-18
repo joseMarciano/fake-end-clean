c1544baf7b462df2f47cd5641e93dfaa6c1d9439
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-17 21:30:50 -0300

    test: ensure AddProjectController returns 200 on AddProject success

--- src/presentation/controllers/project/add/addProjectController.spec.ts
+++ src/presentation/controllers/project/add/addProjectController.spec.ts
@@ -1,4 +1,5 @@
-import { AddProject, AddProjectModel } from 'src/domain/usecases/project/add/AddProject'
+import { AddProject, AddProjectModel } from '../../../../domain/usecases/project/add/AddProject'
+import { ok } from '../../../../presentation/helper/httpHelper'
 import { Project } from '../../../../domain/model/Project'
 import { HttpRequest } from '../../../../presentation/protocols'
 import { AddProjectController } from './AddProjectController'
@@ -57,5 +58,12 @@ describe('AddProjectController', () => {
       ...makeFakeHttpRequest().body,
       ...makeFakeHttpRequest().params
     })
+  })
+  test('Should return an 200 on AddProject success', async () => {
+    const { sut } = makeSut()
+
+    const response = await sut.handle(makeFakeHttpRequest())
+
+    expect(response).toEqual(ok(makeFakeProject()))
   })
 })
