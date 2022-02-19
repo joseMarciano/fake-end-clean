8ca098345d15c05ba8f26b2e36afb68bb7c92be8
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-19 17:08:44 -0300

    feat: ensure DbAppProject returns UserNotFoundError if FindUserByIdRepository fails

--- src/data/usecases/project/DbAddProject.ts
+++ src/data/usecases/project/DbAddProject.ts
@@ -1,7 +1,7 @@
-import { FindUserByIdRepository } from 'src/data/protocols/user/FindUserByIdRepository'
-import { Project } from 'src/domain/model/Project'
-import { AddProject, AddProjectModel } from 'src/domain/usecases/project/add/AddProject'
-import { UserNotFoundError } from 'src/domain/usecases/user/validations/UserNotFoundError'
+import { FindUserByIdRepository } from '../../../data/protocols/user/FindUserByIdRepository'
+import { Project } from '../../../domain/model/Project'
+import { AddProject, AddProjectModel } from '../../../domain/usecases/project/add/AddProject'
+import { UserNotFoundError } from '../../../domain/usecases/user/validations/UserNotFoundError'
 
 export class DbAddProject implements AddProject {
   constructor (
@@ -9,7 +9,10 @@ export class DbAddProject implements AddProject {
   ) {}
 
   async add (projectModel: AddProjectModel): Promise<Project | UserNotFoundError> {
-    await this.findUserByIdRepository.findById(projectModel.userId)
+    const user = await this.findUserByIdRepository.findById(projectModel.userId)
+
+    if (!user) return new UserNotFoundError(`User ${projectModel.userId} not found`)
+
     return await Promise.resolve(null as unknown as Project)
   }
 }
