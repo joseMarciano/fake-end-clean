2beef787583b25d982368696be4e6508192fbb58
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-22 23:20:35 -0300

    feat: ensure DbAuthByToken calls FindUserByEmailRepository with correct value

--- src/data/usecases/user/authentication/DbAuthByToken.ts
+++ src/data/usecases/user/authentication/DbAuthByToken.ts
@@ -1,12 +1,20 @@
+import { FindUserByEmailRepository } from 'src/data/protocols/user/FindUserByEmailRepository'
 import { Decrypter } from '../../../../data/protocols/cryptography/Decrypter'
 import { AuthByToken } from '../../../../domain/usecases/user/authentication/AuthByToken'
 
 export class DbAuthByToken implements AuthByToken {
   constructor (
-    private readonly decrypter: Decrypter
+    private readonly decrypter: Decrypter,
+    private readonly findUserByEmailRespository: FindUserByEmailRepository
   ) {}
 
   async authByToken (token: string): Promise<boolean> {
-    return !!await this.decrypter.decrypt(token)
+    const isAuthenticated = false
+    const decrypted = await this.decrypter.decrypt(token)
+
+    if (!decrypted) return isAuthenticated
+
+    await this.findUserByEmailRespository.findByEmail(decrypted.email)
+    return await Promise.resolve(null as any)
   }
 }
