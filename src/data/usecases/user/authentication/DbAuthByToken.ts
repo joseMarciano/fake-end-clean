40ddd5dd4d48544a87d10e8a3ae2027675ce4959
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-22 23:37:47 -0300

    feat: ensure DbAuthByToken calls FindUserAccessRepository with correct value

--- src/data/usecases/user/authentication/DbAuthByToken.ts
+++ src/data/usecases/user/authentication/DbAuthByToken.ts
@@ -1,23 +1,26 @@
-import { FindUserByEmailRepository } from 'src/data/protocols/user/FindUserByEmailRepository'
+import { FindUserAccessRepository } from '../../../../data/protocols/user/FindUserAccessRepository'
+import { FindUserByEmailRepository } from '../../../../data/protocols/user/FindUserByEmailRepository'
 import { Decrypter } from '../../../../data/protocols/cryptography/Decrypter'
 import { AuthByToken } from '../../../../domain/usecases/user/authentication/AuthByToken'
 
 export class DbAuthByToken implements AuthByToken {
   constructor (
     private readonly decrypter: Decrypter,
-    private readonly findUserByEmailRespository: FindUserByEmailRepository
+    private readonly findUserByEmailRespository: FindUserByEmailRepository,
+    private readonly findUserAccessRepository: FindUserAccessRepository
   ) {}
 
   async authByToken (token: string): Promise<boolean> {
-    const isAuthenticated = false
-    const decrypted = await this.decrypter.decrypt(token)
+    let isAuthenticated = false
 
+    const decrypted = await this.decrypter.decrypt(token)
     if (!decrypted) return isAuthenticated
 
     const user = await this.findUserByEmailRespository.findByEmail(decrypted.email)
-
     if (!user) return isAuthenticated
 
-    return await Promise.resolve(null as any)
+    isAuthenticated = !!await this.findUserAccessRepository.findUserAccess(user.id, token)
+
+    return isAuthenticated
   }
 }
