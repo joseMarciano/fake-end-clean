6519568720e8948a70b75023480ba50167645629
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-22 23:38:25 -0300

    test: ensure DbAuthByToken calls FindUserAccessRepository with correct value

--- src/data/usecases/user/authentication/dbAuthByToken.spec.ts
+++ src/data/usecases/user/authentication/dbAuthByToken.spec.ts
@@ -1,7 +1,8 @@
-import { FindUserByEmailRepository } from 'src/data/protocols/user/FindUserByEmailRepository'
-import { User } from 'src/domain/model/User'
+import { FindUserByEmailRepository } from '../../../../data/protocols/user/FindUserByEmailRepository'
+import { User, UserAccessToken } from '../../../../domain/model/User'
 import { Decrypter } from '../../../../data/protocols/cryptography/Decrypter'
 import { DbAuthByToken } from './DbAuthByToken'
+import { FindUserAccessRepository } from '../../../../data/protocols/user/FindUserAccessRepository'
 
 const makeFakeUser = (): User => ({
   id: 'any_id',
@@ -11,6 +12,22 @@ const makeFakeUser = (): User => ({
   password: 'any_password'
 })
 
+const makeFakeUserAccessToken = (): UserAccessToken => ({
+  id: 'any_id',
+  accessToken: 'any_access_token',
+  createdAt: new Date(),
+  userId: 'any_id'
+})
+
+const makeFindUSerAccessRepository = (): FindUserAccessRepository => {
+  class FindUserAccessRepositoryStub implements FindUserAccessRepository {
+    async findUserAccess (_userId: string, _accessToken: string): Promise<UserAccessToken> {
+      return makeFakeUserAccessToken()
+    }
+  }
+
+  return new FindUserAccessRepositoryStub()
+}
 const makeFindUserByEmail = (): FindUserByEmailRepository => {
   class FindUserByEmailRepositoryStub implements FindUserByEmailRepository {
     async findByEmail (email: string): Promise<User> {
@@ -38,16 +55,19 @@ interface SutTypes {
   sut: DbAuthByToken
   decrypterStub: Decrypter
   findUserByEmailStub: FindUserByEmailRepository
+  findUserAccessRepositoryStub: FindUserAccessRepository
 }
 const makeSut = (): SutTypes => {
+  const findUserAccessRepositoryStub = makeFindUSerAccessRepository()
   const findUserByEmailStub = makeFindUserByEmail()
   const decrypterStub = makeDecrypter()
-  const sut = new DbAuthByToken(decrypterStub, findUserByEmailStub)
+  const sut = new DbAuthByToken(decrypterStub, findUserByEmailStub, findUserAccessRepositoryStub)
 
   return {
     sut,
     decrypterStub,
-    findUserByEmailStub
+    findUserByEmailStub,
+    findUserAccessRepositoryStub
   }
 }
 
@@ -104,5 +124,15 @@ describe('DbAuthByToken', () => {
     const result = await sut.authByToken('any_token')
 
     expect(result).toBe(false)
+  })
+
+  test('Should call FindUserAccessRepository with correct value', async () => {
+    const { sut, findUserAccessRepositoryStub } = makeSut()
+
+    const findUserAccessRepositoryStubSpy =
+     jest.spyOn(findUserAccessRepositoryStub, 'findUserAccess')
+    await sut.authByToken('any_token')
+
+    expect(findUserAccessRepositoryStubSpy).toHaveBeenCalledWith('any_id', 'any_token')
   })
 })
