4f2c59d3549edb65bd0e430fe0750450da48c915
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-22 21:36:58 -0300

    feat: add projectRoute

--- src/main/config/routes/routes.ts
+++ src/main/config/routes/routes.ts
@@ -1,10 +1,12 @@
 import { Express, Router } from 'express'
+import projectRoute from '../../../main/routes/project/projectRoute'
 import loginRoute from '../../routes/login/loginRoute'
 
 export const setRoutes = (app: Express): void => {
   const router = Router()
 
   loginRoute(router)
+  projectRoute(router)
 
   app
     .use(process.env.DEFAULT_PATH as string, router)
