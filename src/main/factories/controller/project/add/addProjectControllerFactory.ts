9416eb90a53c9da5d41a6ced6a4c82466d6002c5
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-22 21:35:54 -0300

    feat: add validationComposite

--- src/main/factories/controller/project/add/addProjectControllerFactory.ts
+++ src/main/factories/controller/project/add/addProjectControllerFactory.ts
@@ -1,13 +1,10 @@
 import { makeDbAddProject } from '../../../../../main/factories/usecases/project/dbAddProject'
 import { AddProjectController } from '../../../../../presentation/controllers/project/add/AddProjectController'
-import { Controller, Validator } from '../../../../../presentation/protocols'
+import { Controller } from '../../../../../presentation/protocols'
+import { makeValidationComposite } from './addProjectValidationCompositeFactory'
 
 export const makeAddProjectController = (): Controller => {
-  class Stub implements Validator {
-    validate (input: any): Error | null {
-      return null
-    }
-  }
   const addProject = makeDbAddProject()
-  return new AddProjectController(addProject, new Stub())
+  const validationComposite = makeValidationComposite()
+  return new AddProjectController(addProject, validationComposite)
 }
