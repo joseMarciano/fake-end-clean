4dd7845f6bc8ae5d21ee0410ae78e4732ce71a32
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-26 12:54:25 -0300

    feat: ensure middlewaresAdapter don't call next() if Controller retuns !== 2xx

--- src/main/adapters/express/middlewaresAdapter.test.ts
+++ src/main/adapters/express/middlewaresAdapter.test.ts
@@ -1,9 +1,10 @@
-import { noContent } from '../../../presentation/helper/httpHelper'
-import { Controller, HttpRequest, HttpResponse } from '../../../presentation/protocols'
 import request from 'supertest'
 import { app } from '../../config/app'
+import { randomUUID } from 'crypto'
+import { Controller, HttpRequest, HttpResponse } from '../../../presentation/protocols'
 import { middlewaresAdapter } from './middlewaresAdapter'
 import { Request, Response } from 'express'
+import { noContent, serverError } from '../../../presentation/helper/httpHelper'
 
 const makeControllerStub = (): Controller => {
   class ControllerStub implements Controller {
@@ -16,18 +17,38 @@ const makeControllerStub = (): Controller => {
 }
 
 describe('middlewaresApdater', () => {
+  let randomUrl = ''
+  beforeEach(() => {
+    randomUrl = '/' + randomUUID()
+  })
+
   test('Should call next() if Controller returns 2xx', async () => {
     const controllerStub = makeControllerStub()
 
     app.use(middlewaresAdapter(controllerStub))
-      .post('/fake-request', (_req: Request, res: Response) => {
+      .post(randomUrl, (_req: Request, res: Response) => {
         return res.status(200).json(_req.body)
       })
 
     await request(app)
-      .post('/fake-request')
+      .post(randomUrl)
       .send({ message: 'Expect call next function' })
       .expect({ message: 'Expect call next function' })
       .expect(200)
+  })
+
+  test('Should not call next() if Controller returns !== 2xx', async () => {
+    const controllerStub = makeControllerStub()
+
+    jest.spyOn(controllerStub, 'handle').mockResolvedValueOnce(serverError(new Error('Next function was not called')))
+
+    app.use(middlewaresAdapter(controllerStub))
+      .post(randomUrl, (_req: Request, res: Response) => {
+        return res.status(200).json({ message: 'Next function was called' })
+      })
+
+    await request(app)
+      .post(randomUrl)
+      .expect({ statusCode: 500, error: 'Next function was not called' })
   })
 })
