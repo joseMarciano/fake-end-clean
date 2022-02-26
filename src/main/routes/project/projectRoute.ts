fdcad221c93851d36de5d6dd501649c9ced1839c
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-26 15:35:10 -0300

    refactor: add new strategy to a projectRoute

--- src/main/routes/project/projectRoute.ts
+++ src/main/routes/project/projectRoute.ts
@@ -2,6 +2,5 @@ import { Router } from 'express'
 import { makeAddProjectController } from '../../../main/factories/controller/project/add/addProjectControllerFactory'
 import { controllerAdapter } from '../../../main/adapters/controllers/controllerAdapter'
 
-export default (router: Router): void => {
-  router.post('/project', controllerAdapter(makeAddProjectController()))
-}
+export default Router()
+  .post('/', controllerAdapter(makeAddProjectController()))
