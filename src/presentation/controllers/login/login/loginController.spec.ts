6d1f5c59f440286a798f4f355cef814b42d7dd7a
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-27 19:46:07 -0300

    test: ensure LoginController calls Validator with correct values

--- src/presentation/controllers/login/login/loginController.spec.ts
+++ src/presentation/controllers/login/login/loginController.spec.ts
@@ -1,5 +1,5 @@
 import { LoginUser, LoginUserModel, AccessToken } from '../../../../domain/usecases/user/authentication/LoginUser'
-import { HttpRequest } from '../../../../presentation/protocols'
+import { HttpRequest, Validator } from '../../../../presentation/protocols'
 import { LoginController } from './LoginController'
 
 const makeFakeHttpRequest = (): HttpRequest => ({
@@ -8,6 +8,16 @@ const makeFakeHttpRequest = (): HttpRequest => ({
     password: 'any_password'
   }
 })
+
+const makeHttpValidator = (): Validator => {
+  class ValidatorStub implements Validator {
+    validate (input: any): Error | null {
+      return null
+    }
+  }
+
+  return new ValidatorStub()
+}
 
 const makeDbLoginUser = (): LoginUser => {
   class DbLoginUserStub implements LoginUser {
@@ -25,14 +35,18 @@ const makeDbLoginUser = (): LoginUser => {
 interface SutTypes {
   sut: LoginController
   dbLoginUserStub: LoginUser
+  validatorStub: Validator
+
 }
 const makeSut = (): SutTypes => {
+  const validatorStub = makeHttpValidator()
   const dbLoginUserStub = makeDbLoginUser()
-  const sut = new LoginController(dbLoginUserStub)
+  const sut = new LoginController(dbLoginUserStub, validatorStub)
 
   return {
     sut,
-    dbLoginUserStub
+    dbLoginUserStub,
+    validatorStub
   }
 }
 
@@ -41,9 +55,17 @@ describe('LoginController', () => {
     const { sut, dbLoginUserStub } = makeSut()
 
     const loginSpy = jest.spyOn(dbLoginUserStub, 'login')
-
     await sut.handle(makeFakeHttpRequest())
 
     expect(loginSpy).toHaveBeenCalledWith(makeFakeHttpRequest().body)
+  })
+
+  test('Should call Validator with correct values', async () => {
+    const { sut, validatorStub } = makeSut()
+
+    const validatorSpy = jest.spyOn(validatorStub, 'validate')
+    await sut.handle(makeFakeHttpRequest())
+
+    expect(validatorSpy).toHaveBeenCalledWith(makeFakeHttpRequest())
   })
 })
