79b22a8604d354f9866270ae177ef9cf76a84daf
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-27 13:20:32 -0300

    feat: ensure AuthController calls SetUserContext with correct values

--- src/presentation/controllers/authentication/AuthController.ts
+++ src/presentation/controllers/authentication/AuthController.ts
@@ -1,17 +1,21 @@
 import { noContent, serverError, unauthorized } from '../../../presentation/helper/httpHelper'
 import { AuthByToken } from '../../../domain/usecases/user/authentication/AuthByToken'
 import { Controller, HttpRequest, HttpResponse } from '../../../presentation/protocols'
+import { SetUserContext } from 'src/data/protocols/application/UserContext'
 
 export class AuthController implements Controller {
   constructor (
-    private readonly auth: AuthByToken
+    private readonly auth: AuthByToken,
+    private readonly applicationContext: SetUserContext
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const isAuthenticated = await this.auth.authByToken(httpRequest.headers?.authorization)
+      const user = await this.auth.authByToken(httpRequest.headers?.authorization)
 
-      if (!isAuthenticated) return unauthorized()
+      if (!user) return unauthorized()
+
+      await this.applicationContext.setUser(user)
 
       return noContent()
     } catch (error) {
