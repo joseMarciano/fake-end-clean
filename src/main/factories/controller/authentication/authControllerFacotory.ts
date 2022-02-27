62416a237aef20b3fa31af7482c1981616697ce7
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-27 13:45:03 -0300

    feat: add ApplicaitonContext on AuthController

--- src/main/factories/controller/authentication/authControllerFacotory.ts
+++ src/main/factories/controller/authentication/authControllerFacotory.ts
@@ -1,8 +1,9 @@
 import { Controller } from '../../../../presentation/protocols'
 import { AuthController } from '../../../../presentation/controllers/authentication/AuthController'
 import { makeAuthByToken } from '../../usecases/authentication/authByTokenFactory'
+import { applicationContext } from '../../application/applicationContextFactory'
 
 export const makeAuthControllerFactory = (): Controller => {
   const authByToken = makeAuthByToken()
-  return new AuthController(authByToken)
+  return new AuthController(authByToken, applicationContext)
 }
