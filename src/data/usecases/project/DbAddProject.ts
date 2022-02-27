074354d695f7e70f513278054cbf17b7b185c63a
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-27 12:33:50 -0300

    refactor: remove findUserByIdRepository

--- src/data/usecases/project/DbAddProject.ts
+++ src/data/usecases/project/DbAddProject.ts
@@ -1,22 +1,15 @@
 import { Encrypter } from '../../../data/protocols/cryptography/Encrypter'
 import { AddProjectRepository } from '../../../data/protocols/project/AddProjectRepository'
-import { FindUserByIdRepository } from '../../../data/protocols/user/FindUserByIdRepository'
 import { Project } from '../../../domain/model/Project'
 import { AddProject, AddProjectModel } from '../../../domain/usecases/project/add/AddProject'
-import { UserNotFoundError } from '../../../domain/usecases/user/validations/UserNotFoundError'
 
 export class DbAddProject implements AddProject {
   constructor (
-    private readonly findUserByIdRepository: FindUserByIdRepository,
     private readonly addProjectRepository: AddProjectRepository,
     private readonly encrypter: Encrypter
   ) {}
 
-  async add (projectModel: AddProjectModel): Promise<Project | UserNotFoundError> {
-    const user = await this.findUserByIdRepository.findById(projectModel.userId)
-
-    if (!user) return new UserNotFoundError(`User ${projectModel.userId} not found`)
-
+  async add (projectModel: AddProjectModel): Promise<Project> {
     const secretKey = await this.encrypter.encrypt({
       ...projectModel,
       createdAt: new Date()
