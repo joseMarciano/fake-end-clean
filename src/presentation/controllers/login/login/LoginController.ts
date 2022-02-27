9158f13503491f822b29cd33178ea10a3109ceb7
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-27 19:44:47 -0300

    feat: ensure LoginController calls Validator with correct values

--- src/presentation/controllers/login/login/LoginController.ts
+++ src/presentation/controllers/login/login/LoginController.ts
@@ -1,12 +1,14 @@
 import { LoginUser } from '../../../../domain/usecases/user/authentication/LoginUser'
-import { Controller, HttpRequest, HttpResponse } from '../../../../presentation/protocols'
+import { Controller, HttpRequest, HttpResponse, Validator } from '../../../../presentation/protocols'
 
 export class LoginController implements Controller {
   constructor (
-    private readonly dbLoginUser: LoginUser
+    private readonly dbLoginUser: LoginUser,
+    private readonly validator: Validator
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    this.validator.validate(httpRequest)
     await this.dbLoginUser.login(httpRequest.body)
     return null as any
   }
