5dfbca505dc1f3d1690983f4f7439d38a1d1b362
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-01 20:08:53 -0300

    refactor: change import of uuid

--- src/infra/cryptography/uui/UUIDAdapter.ts
+++ src/infra/cryptography/uui/UUIDAdapter.ts
@@ -1,7 +1,7 @@
 import { RandomStringGenerator } from '../../../data/protocols/cryptography/RandomStringGenerator'
-import uuid from 'uuid'
+import { v4 } from 'uuid'
 export class UUIDAdapter implements RandomStringGenerator {
   async generateRandomString (): Promise<string> {
-    return await Promise.resolve(uuid.v4())
+    return await Promise.resolve(v4())
   }
 }
