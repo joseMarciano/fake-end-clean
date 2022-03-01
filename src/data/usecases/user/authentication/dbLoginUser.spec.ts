7f3e63f6b590bc9f8023cc7b250ba3e3dcdcc93e
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-28 21:40:04 -0300

    test: ensure DbLoginUser calls FindUserByEmailRepository with correct values

---
+++ src/data/usecases/user/authentication/dbLoginUser.spec.ts
@@ -0,0 +1,53 @@
+import { User } from '../../../../domain/model/User'
+import { LoginUserModel } from '../../../../domain/usecases/user/authentication/LoginUser'
+import { FindUserByEmailRepository } from '../../../../data/protocols/user/FindUserByEmailRepository'
+import { DbLoginUser } from './DbLoginUser'
+
+const makeFakeUser = (): User => ({
+  id: 'any_id',
+  email: 'any_email',
+  isActive: false,
+  name: 'any_name',
+  password: 'any_password'
+})
+
+const makeFakeLoginUserModel = (): LoginUserModel => ({
+  email: 'any_email',
+  password: 'any_password'
+})
+
+const makeFindUserByEmailRepository = (): FindUserByEmailRepository => {
+  class FindUserByEmailRepositoryStub implements FindUserByEmailRepository {
+    async findByEmail (email: string): Promise<User> {
+      return await Promise.resolve(makeFakeUser())
+    }
+  }
+
+  return new FindUserByEmailRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbLoginUser
+  findUserByEmailRepositoryStub: FindUserByEmailRepository
+}
+
+const makeSut = (): SutTypes => {
+  const findUserByEmailRepositoryStub = makeFindUserByEmailRepository()
+  const sut = new DbLoginUser(findUserByEmailRepositoryStub)
+
+  return {
+    sut,
+    findUserByEmailRepositoryStub
+  }
+}
+
+describe('DbLoginUser', () => {
+  test('Should call FindUserByEmailRepository with correct value', async () => {
+    const { sut, findUserByEmailRepositoryStub } = makeSut()
+
+    const findUserByEmailStub = jest.spyOn(findUserByEmailRepositoryStub, 'findByEmail')
+    await sut.login(makeFakeLoginUserModel())
+
+    expect(findUserByEmailStub).toHaveBeenCalledWith('any_email')
+  })
+})
