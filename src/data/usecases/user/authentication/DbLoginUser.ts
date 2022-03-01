43b8d8616e16ca5605300d4a26ab3889afd4e8d3
joseMarciano <marcianojosepaulo@gmail.com>
2022-02-28 22:00:56 -0300

    feat: ensure DbLoginUser calls HashCompare with correct values

--- src/data/usecases/user/authentication/DbLoginUser.ts
+++ src/data/usecases/user/authentication/DbLoginUser.ts
@@ -1,16 +1,20 @@
 import { LoginUserError } from '../../../../domain/usecases/user/validations/LoginUserError'
 import { AccessToken, LoginUser, LoginUserModel } from '../../../../domain/usecases/user/authentication/LoginUser'
 import { FindUserByEmailRepository } from '../../../../data/protocols/user/FindUserByEmailRepository'
+import { HashCompare } from '../../../../data/protocols/cryptography/HashCompare'
 
 export class DbLoginUser implements LoginUser {
   constructor (
-    private readonly findUserByEmailRepository: FindUserByEmailRepository
+    private readonly findUserByEmailRepository: FindUserByEmailRepository,
+    private readonly hasherCompare: HashCompare
   ) {}
 
   async login (userLoginModel: LoginUserModel): Promise<AccessToken | LoginUserError> {
     const user = await this.findUserByEmailRepository.findByEmail(userLoginModel.email)
 
     if (!user) return new LoginUserError('Email or password are incorrects')
+
+    await this.hasherCompare.compare(userLoginModel.password, user.password as string)
 
     return null as any
   }
