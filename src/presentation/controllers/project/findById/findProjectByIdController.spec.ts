d568e2d66f35383a01dbf6e06469af8f9ee20b17
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-03 23:58:55 -0300

    test: create FindProjectByIdController basic functionalities

--- src/presentation/controllers/project/findById/findProjectByIdController.spec.ts
+++ src/presentation/controllers/project/findById/findProjectByIdController.spec.ts
@@ -1,7 +1,7 @@
-import { ok } from '../../../../presentation/helper/httpHelper'
+import { badRequest, ok, serverError } from '../../../../presentation/helper/httpHelper'
 import { FindProjectById } from '../../../../domain/usecases/project/find/FindProjectById'
 import { ProjectModel } from '../../../../domain/usecases/project/find/ProjectModel'
-import { HttpRequest } from '../../../../presentation/protocols'
+import { HttpRequest, Validator } from '../../../../presentation/protocols'
 import { FindProjectByIdController } from './FindProjectByIdController'
 
 const makeFakeHttpRequest = (): HttpRequest => ({
@@ -26,17 +26,30 @@ const makeFindProjectById = (): FindProjectById => {
 
   return new FindProjectByIdStub()
 }
+
+const makeValidation = (): Validator => {
+  class ValidatorStub implements Validator {
+    validate (input: any): Error | null {
+      return null
+    };
+  }
+
+  return new ValidatorStub()
+}
 interface SutTypes {
   sut: FindProjectByIdController
   findProjectByIdStub: FindProjectById
+  validtionStub: Validator
 }
 const makeSut = (): SutTypes => {
+  const validtionStub = makeValidation()
   const findProjectByIdStub = makeFindProjectById()
-  const sut = new FindProjectByIdController(findProjectByIdStub)
+  const sut = new FindProjectByIdController(findProjectByIdStub, validtionStub)
 
   return {
     sut,
-    findProjectByIdStub
+    findProjectByIdStub,
+    validtionStub
   }
 }
 
@@ -57,5 +70,41 @@ describe('FindProjectById', () => {
     const response = await sut.handle(makeFakeHttpRequest())
 
     expect(response).toEqual(ok(makeFakeProjectModel()))
+  })
+
+  test('Should return 500 on FindProjectById throws', async () => {
+    const { sut, findProjectByIdStub } = makeSut()
+
+    jest.spyOn(findProjectByIdStub, 'findById').mockImplementationOnce(() => { throw new Error() })
+    const response = await sut.handle(makeFakeHttpRequest())
+
+    expect(response).toEqual(serverError(new Error()))
+  })
+
+  test('Should call Validator with correct values', async () => {
+    const { sut, validtionStub } = makeSut()
+
+    const validateSpy = jest.spyOn(validtionStub, 'validate')
+    await sut.handle(makeFakeHttpRequest())
+
+    expect(validateSpy).toHaveBeenCalledWith(makeFakeHttpRequest())
+  })
+
+  test('Should return 400 if Validator returns an Error', async () => {
+    const { sut, validtionStub } = makeSut()
+
+    jest.spyOn(validtionStub, 'validate').mockReturnValueOnce(new Error())
+    const response = await sut.handle(makeFakeHttpRequest())
+
+    expect(response).toEqual(badRequest(new Error()))
+  })
+
+  test('Should return 500 if Validator throws', async () => {
+    const { sut, validtionStub } = makeSut()
+
+    jest.spyOn(validtionStub, 'validate').mockImplementationOnce(() => { throw new Error() })
+    const response = await sut.handle(makeFakeHttpRequest())
+
+    expect(response).toEqual(serverError(new Error()))
   })
 })
