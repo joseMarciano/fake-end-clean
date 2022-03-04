453c6baf44d764c29611d3e4942523e992b5a5af
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-03 23:57:53 -0300

    feat: create FindProjectByIdController basic functionalities

--- src/presentation/controllers/project/findById/FindProjectByIdController.ts
+++ src/presentation/controllers/project/findById/FindProjectByIdController.ts
@@ -1,13 +1,22 @@
-import { ok } from '../../../../presentation/helper/httpHelper'
+import { badRequest, ok, serverError } from '../../../../presentation/helper/httpHelper'
 import { FindProjectById } from '../../../../domain/usecases/project/find/FindProjectById'
-import { Controller, HttpRequest, HttpResponse } from '../../../../presentation/protocols'
+import { Controller, HttpRequest, HttpResponse, Validator } from '../../../../presentation/protocols'
 
 export class FindProjectByIdController implements Controller {
   constructor (
-    private readonly findProjectById: FindProjectById
+    private readonly findProjectById: FindProjectById,
+    private readonly validator: Validator
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    return ok(await this.findProjectById.findById(httpRequest.paths.id))
+    try {
+      const isValid = this.validator.validate(httpRequest)
+
+      if (isValid) return badRequest(isValid)
+
+      return ok(await this.findProjectById.findById(httpRequest.paths.id))
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
