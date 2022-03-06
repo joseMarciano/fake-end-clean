4a77d2bd09c796c1dbce9a55e4c97b93237a847b
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-06 16:12:02 -0300

    feat: insert FindProjectByIdController in route

--- src/main/routes/project/projectRoute.ts
+++ src/main/routes/project/projectRoute.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 import { makeAddProjectController } from '../../../main/factories/controller/project/add/addProjectControllerFactory'
 import { controllerAdapter } from '../../../main/adapters/controllers/controllerAdapter'
+import { makeFindProjectByIdController } from '../../../main/factories/controller/project/findById/findProjectByIdControllerFactory'
 
 export default Router()
   .post('/', controllerAdapter(makeAddProjectController()))
+  .get('/:id', controllerAdapter(makeFindProjectByIdController()))
