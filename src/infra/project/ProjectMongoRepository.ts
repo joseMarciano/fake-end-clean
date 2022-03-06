14d2c3b500afb1e8bb83433c9a7ea3491f5db60f
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-06 15:21:35 -0300

    feat: create FindProjectByIdREpository

--- src/infra/project/ProjectMongoRepository.ts
+++ src/infra/project/ProjectMongoRepository.ts
@@ -1,10 +1,18 @@
+import { FindProjectByIdRepository } from '../../data/protocols/project/FindProjectByIdRepository'
 import { GetUserContext } from '../../data/protocols/application/UserContext'
 import { AddProjectRepository } from '../../data/protocols/project/AddProjectRepository'
 import { Project } from '../../domain/model/Project'
 import { AddProjectModel } from '../../domain/usecases/project/add/AddProject'
 import { MongoHelper } from '../db/mongo/mongoHelper'
+import { ProjectModel } from '../../domain/usecases/project/find/ProjectModel'
+import { ObjectId } from 'mongodb'
 
-export class ProjectMongoRepository implements AddProjectRepository {
+interface BasicRepository
+  extends
+  AddProjectRepository,
+  FindProjectByIdRepository
+{}
+export class ProjectMongoRepository implements BasicRepository {
   constructor (private readonly applicationContext: GetUserContext) {}
 
   async addProject (projectModel: AddProjectModel): Promise<Project> {
@@ -18,6 +26,25 @@ export class ProjectMongoRepository implements AddProjectRepository {
     })
 
     const { _id, ...obj } = await collection.findOne({ _id: result.insertedId }) as any
+
+    return {
+      id: _id.toString(),
+      ...obj
+    }
+  }
+
+  async findById (id: string): Promise<ProjectModel> {
+    const userContext = await this.applicationContext.getUser()
+    const collection = await MongoHelper.getCollection('projects')
+
+    const projectDocument = await collection.findOne({
+      user: userContext.id,
+      _id: new ObjectId(id)
+    })
+
+    if (!projectDocument) return null as any
+
+    const { _id, user, ...obj } = projectDocument as any
 
     return {
       id: _id.toString(),
