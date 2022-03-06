c83d3f4ccb23252311e2936a6c72d6d24855e0c8
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-06 18:52:19 -0300

    test: ensure PageProjectController calls PageProjectwith correct values

---
+++ src/presentation/controllers/project/page/pageProjectController.spec.ts
@@ -0,0 +1,57 @@
+import { HttpRequest } from '../../../../presentation/protocols'
+import { ProjectModel } from '../../../../domain/usecases/project/find/ProjectModel'
+import { PageProjectController } from './PageProjectController'
+import { PageProject } from '../../../../domain/usecases/project/find/PageProject'
+import { Page, Pageable } from '../../../../domain/usecases/commons/Page'
+
+const makeFakeHttpRequest = (): HttpRequest => ({
+  params: {
+    offset: 0,
+    limit: 20
+  }
+})
+const makeFakeProject = (): ProjectModel => ({
+  id: 'any_id',
+  description: 'any_description',
+  secretKey: 'any_secret_key',
+  title: 'any_title'
+})
+const makeFakePageProject = (): Page<ProjectModel> => ({
+  offset: 0,
+  limit: 20,
+  content: [makeFakeProject()]
+})
+
+const makePageProject = (): PageProject => {
+  class PageProjectStub implements PageProject {
+    async page (_pageable: Pageable): Promise<Page<ProjectModel>> {
+      return await Promise.resolve(makeFakePageProject())
+    }
+  }
+
+  return new PageProjectStub()
+}
+
+interface SutTypes {
+  sut: PageProjectController
+  dbPageProjectStub: PageProject
+}
+
+const makeSut = (): SutTypes => {
+  const dbPageProjectStub = makePageProject()
+  const sut = new PageProjectController(dbPageProjectStub)
+
+  return {
+    sut,
+    dbPageProjectStub
+  }
+}
+
+describe('PageProjectController', () => {
+  test('Should call dbPageProject with correct values', async () => {
+    const { sut, dbPageProjectStub } = makeSut()
+    const pageSpy = jest.spyOn(dbPageProjectStub, 'page')
+    await sut.handle(makeFakeHttpRequest())
+    expect(pageSpy).toHaveBeenCalledWith({ offset: 0, limit: 20 })
+  })
+})
