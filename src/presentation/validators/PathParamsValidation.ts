dc596eaad76747df8020a90cd2d37cf330bf2838
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-06 14:41:20 -0300

    feat: create PathParamsValidation

---
+++ src/presentation/validators/PathParamsValidation.ts
@@ -0,0 +1,11 @@
+import { Validator } from '../protocols'
+
+export class PathParamValidation implements Validator {
+  constructor (
+    private readonly validator: Validator
+  ) {}
+
+  validate (input: any): Error | null {
+    return this.validator.validate(input.path)
+  }
+}
