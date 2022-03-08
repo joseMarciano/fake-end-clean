e9d46f40f3619ed49c61dfd4d33b0e4620b0cb5e
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-07 22:51:19 -0300

    feat: insert PageProjectController in router

--- src/main/routes/project/projectRoute.ts
+++ src/main/routes/project/projectRoute.ts
@@ -2,7 +2,9 @@ import { Router } from 'express'
 import { makeAddProjectController } from '../../../main/factories/controller/project/add/addProjectControllerFactory'
 import { controllerAdapter } from '../../../main/adapters/controllers/controllerAdapter'
 import { makeFindProjectByIdController } from '../../../main/factories/controller/project/findById/findProjectByIdControllerFactory'
+import { makePageProjectController } from '../../../main/factories/controller/project/page/pageProjectControllerFactory'
 
 export default Router()
   .post('/', controllerAdapter(makeAddProjectController()))
+  .get('/', controllerAdapter(makePageProjectController()))
   .get('/:id', controllerAdapter(makeFindProjectByIdController()))
