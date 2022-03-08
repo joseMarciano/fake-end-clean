1227543f58ba150fcd4fef9db5ff5365fce1e7e5
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-07 22:16:41 -0300

    refactor: add default value in arrayResults

--- src/domain/usecases/commons/Page.ts
+++ src/domain/usecases/commons/Page.ts
@@ -12,7 +12,7 @@ export interface Page<T> {
 }
 
 export const PageUtils = {
-  buildPage<T> (pageable: Pageable, total: number, arrayResults: T[]): Page<T> {
+  buildPage<T> (pageable: Pageable, total: number, arrayResults: T[] = []): Page<T> {
     const hasNext = total - ((pageable.offset * pageable.limit) + arrayResults.length) > 0
     return {
       total,
