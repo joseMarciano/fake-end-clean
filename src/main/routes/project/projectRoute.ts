d979c613ce6b0fec18b8502069414feee5a6a864
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-09 01:11:18 -0300

    feat: insert DeleteProjectById in router

--- src/main/routes/project/projectRoute.ts
+++ src/main/routes/project/projectRoute.ts
@@ -3,8 +3,10 @@ import { makeAddProjectController } from '../../../main/factories/controller/pro
 import { controllerAdapter } from '../../../main/adapters/controllers/controllerAdapter'
 import { makeFindProjectByIdController } from '../../../main/factories/controller/project/findById/findProjectByIdControllerFactory'
 import { makePageProjectController } from '../../../main/factories/controller/project/page/pageProjectControllerFactory'
+import { makeDeleteProjectByIdController } from '../../../main/factories/controller/project/remove/deleteProjectByIdControllerFactory'
 
 export default Router()
   .post('/', controllerAdapter(makeAddProjectController()))
   .get('/', controllerAdapter(makePageProjectController()))
   .get('/:id', controllerAdapter(makeFindProjectByIdController()))
+  .delete('/:id', controllerAdapter(makeDeleteProjectByIdController()))
