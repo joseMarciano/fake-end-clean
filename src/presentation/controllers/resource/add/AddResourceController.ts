a2525b5db55a16f3bbe4a8fd469c1c38e7681663
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-10 00:04:30 -0300

    feat: create AddResourceController

---
+++ src/presentation/controllers/resource/add/AddResourceController.ts
@@ -0,0 +1,29 @@
+import { AddResource, AddResourceModel } from '../../../../domain/usecases/resource/add/AddResource'
+import { badRequest, ok, serverError } from '../../../helper/httpHelper'
+import { Controller, HttpRequest, HttpResponse, Validator } from '../../../protocols'
+
+export class AddResourceController implements Controller {
+  constructor (
+    private readonly addResource: AddResource,
+    private readonly validator: Validator
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const error = this.validator.validate(httpRequest)
+
+      if (error) return badRequest(error)
+
+      const resourceModel: AddResourceModel = {
+        ...httpRequest.body,
+        project: httpRequest.paths.projectId
+      }
+
+      const result = await this.addResource.add(resourceModel)
+
+      return ok(result)
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
