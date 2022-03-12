c02270567abb0fffe32f81c67379b07f7259efd7
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-12 19:49:03 -0300

    feat: create EditProjectRepository

--- src/infra/project/ProjectMongoRepository.ts
+++ src/infra/project/ProjectMongoRepository.ts
@@ -1,7 +1,7 @@
 import { FindProjectByIdRepository } from '../../data/protocols/project/FindProjectByIdRepository'
 import { GetUserContext } from '../../data/protocols/application/UserContext'
 import { AddProjectRepository } from '../../data/protocols/project/AddProjectRepository'
-import { Project } from '../../domain/model/Project'
+import { nonUpdatableFields, Project } from '../../domain/model/Project'
 import { AddProjectModel } from '../../domain/usecases/project/add/AddProject'
 import { MongoHelper } from '../db/mongo/mongoHelper'
 import { ProjectModel } from '../../domain/usecases/project/find/ProjectModel'
@@ -9,13 +9,15 @@ import { ObjectId } from 'mongodb'
 import { PageProjectRepository } from '../../data/protocols/project/PageProjectRepository'
 import { Pageable, Page, PageUtils } from '../../domain/usecases/commons/Page'
 import { DeleteProjectByIdRepository } from '../../data/protocols/project/DeleteProjectByIdRepository'
+import { EditProjectRepository } from 'src/data/protocols/project/EditProjectRepository'
 
 interface BasicRepository
   extends
   AddProjectRepository,
   FindProjectByIdRepository,
   PageProjectRepository,
-  DeleteProjectByIdRepository
+  DeleteProjectByIdRepository,
+  EditProjectRepository
 {}
 export class ProjectMongoRepository implements BasicRepository {
   constructor (private readonly applicationContext: GetUserContext) {}
@@ -88,5 +90,29 @@ export class ProjectMongoRepository implements BasicRepository {
     const collection = await MongoHelper.getCollection('projects')
 
     await collection.deleteOne({ user: userContext.id, _id: new ObjectId(id) })
+  }
+
+  async edit (project: Project): Promise<Project> {
+    const userContext = await this.applicationContext.getUser()
+    const collection = await MongoHelper.getCollection('projects')
+    const projectUpdate = buildUpdateProject()
+
+    await collection.findOneAndUpdate(
+      { _id: new ObjectId(project.id), user: userContext.id },
+      { $set: projectUpdate }
+    )
+
+    return await Promise.resolve(null as any)
+
+    function buildUpdateProject (): any {
+      const projectUpdate = {} as any
+      const projectFields = Object.keys(project) as any
+
+      projectFields
+        .filter((field: string) => !nonUpdatableFields.includes(field))
+        .forEach((projectField: string) => { projectUpdate[projectField] = project[projectField as keyof Project] })
+
+      return projectUpdate
+    }
   }
 }
