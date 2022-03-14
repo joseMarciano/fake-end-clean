60f3d167da3b37fd55b0be0792086d9726386043
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-14 15:21:37 -0300

    feat: insert DeleteResourceByIdController in route

--- src/main/routes/resource/resourceRoute.ts
+++ src/main/routes/resource/resourceRoute.ts
@@ -2,7 +2,9 @@ import { Router } from 'express'
 import { makeFindResourceByProjectIdController } from '../../../main/factories/controller/resource/findResourceByProjectId/findResourceByProjectIdControllerFactory'
 import { makeAddResourceController } from '../../../main/factories/controller/resource/add/addResourceControllerFactory'
 import { controllerAdapter } from '../../adapters/controllers/controllerAdapter'
+import { makeDeleteResourceByIdController } from '../../../main/factories/controller/resource/remove/deleteResourceByIdControllerFactory'
 
 export default Router()
   .post('/:projectId', controllerAdapter(makeAddResourceController()))
   .get('/:projectId', controllerAdapter(makeFindResourceByProjectIdController()))
+  .delete('/:id', controllerAdapter(makeDeleteResourceByIdController()))
