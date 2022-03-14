836cfe89ba0582c2311b19763ed3cce933ae34b0
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-14 16:34:09 -0300

    refactor: remove field in nonUpdatableFields const

--- src/domain/model/Project.ts
+++ src/domain/model/Project.ts
@@ -6,4 +6,4 @@ export interface Project {
   user: string
 }
 
-export const nonUpdatableFields = ['id', 'secretKey', 'createdAt', 'user']
+export const nonUpdatableFields = ['id', 'createdAt', 'user']
