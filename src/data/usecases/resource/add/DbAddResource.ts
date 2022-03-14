201a356414e8b8a4b20477913d2da8f98996c4ac
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-14 11:58:42 -0300

    refactor: change FindResourceByNameRepository to FindResourceByNameAndProjectIdRepository

--- src/data/usecases/resource/add/DbAddResource.ts
+++ src/data/usecases/resource/add/DbAddResource.ts
@@ -3,19 +3,20 @@ import { AddResourceRepository } from '../../../../data/protocols/resource/AddRe
 import { FindProjectByIdRepository } from '../../../../data/protocols/project/FindProjectByIdRepository'
 import { Resource } from '../../../../domain/model/Resource'
 import { AddResourceError } from '../../../../domain/usecases/resource/validations/AddResourceError'
-import { FindResourceByNameRepository } from '../../../../data/protocols/resource/FindResourceByNameRepository'
+import { FindResourceByNameAndProjectIdRepository } from '../../../protocols/resource/FindResourceByNameAndProjectIdRepository'
 
 export class DbAddResource implements AddResource {
   constructor (
     private readonly addResourceRepository: AddResourceRepository,
-    private readonly findResourceByNameRepository: FindResourceByNameRepository,
+    private readonly findResourceByNameRepository: FindResourceByNameAndProjectIdRepository,
     private readonly findProjectByIdRepository: FindProjectByIdRepository
   ) {}
 
   async add (resourceModel: AddResourceModel): Promise<Resource | AddResourceError> {
     const project = await this.findProjectByIdRepository.findById(resourceModel.project)
-    const resource = await this.findResourceByNameRepository.findByName(resourceModel.name)
     if (!project) return new AddResourceError('Project not found')
+
+    const resource = await this.findResourceByNameRepository.findByNameAndProjectId({ name: resourceModel.name, projectId: project.id })
     if (resource) return new AddResourceError('Resource name already exists')
 
     return await this.addResourceRepository.add(resourceModel)
