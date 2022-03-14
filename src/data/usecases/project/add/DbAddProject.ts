defb733a4a5990dfb0c3ceedda1c1eff57bcdc1e
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-14 16:11:18 -0300

    refactor: change secretKey encrypt

--- src/data/usecases/project/add/DbAddProject.ts
+++ src/data/usecases/project/add/DbAddProject.ts
@@ -2,22 +2,20 @@ import { Encrypter } from '../../../protocols/cryptography/Encrypter'
 import { AddProjectRepository } from '../../../protocols/project/AddProjectRepository'
 import { Project } from '../../../../domain/model/Project'
 import { AddProject, AddProjectModel } from '../../../../domain/usecases/project/add/AddProject'
+import { EditProjectRepository } from '../../../../data/protocols/project/EditProjectRepository'
 
 export class DbAddProject implements AddProject {
   constructor (
     private readonly addProjectRepository: AddProjectRepository,
-    private readonly encrypter: Encrypter
+    private readonly encrypter: Encrypter,
+    private readonly editProjectRepository: EditProjectRepository
   ) {}
 
   async add (projectModel: AddProjectModel): Promise<Project> {
-    const secretKey = await this.encrypter.encrypt({
-      ...projectModel,
-      createdAt: new Date()
-    })
+    const project = await this.addProjectRepository.addProject({ ...projectModel, secretKey: '' })
 
-    return await this.addProjectRepository.addProject({
-      ...projectModel,
-      secretKey
-    })
+    const secretKey = await this.encrypter.encrypt(project.id)
+
+    return await this.editProjectRepository.edit({ ...project, secretKey })
   }
 }
