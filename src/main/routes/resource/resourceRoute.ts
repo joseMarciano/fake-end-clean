35ee46bc66550f8c8f077beca9aa8dafee47af57
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-14 14:37:28 -0300

    feat: insert FindResourceByProjectIdController in route

--- src/main/routes/resource/resourceRoute.ts
+++ src/main/routes/resource/resourceRoute.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
+import { makeFindResourceByProjectIdController } from '../../../main/factories/controller/resource/findResourceByProjectId/findResourceByProjectIdControllerFactory'
 import { makeAddResourceController } from '../../../main/factories/controller/resource/add/addResourceControllerFactory'
 import { controllerAdapter } from '../../adapters/controllers/controllerAdapter'
 
 export default Router()
   .post('/:projectId', controllerAdapter(makeAddResourceController()))
+  .get('/:projectId', controllerAdapter(makeFindResourceByProjectIdController()))
