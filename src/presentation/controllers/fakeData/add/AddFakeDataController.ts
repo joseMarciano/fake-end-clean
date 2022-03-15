5d824f7b18bd4a9b5757877b6e52241fb3402a59
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-15 10:53:27 -0300

    feat: ensure AddFakeDataContoller calls Validator with correct values

--- src/presentation/controllers/fakeData/add/AddFakeDataController.ts
+++ src/presentation/controllers/fakeData/add/AddFakeDataController.ts
@@ -1,14 +1,16 @@
 import { ok, serverError } from '../../../../presentation/helper/httpHelper'
 import { AddFakeData } from '../../../../domain/usecases/fakeData/add/AddFakeData'
-import { Controller, HttpRequest, HttpResponse } from '../../../../presentation/protocols'
+import { Controller, HttpRequest, HttpResponse, Validator } from '../../../../presentation/protocols'
 
 export class AddFakeDataController implements Controller {
   constructor (
-    private readonly addFakeData: AddFakeData
+    private readonly addFakeData: AddFakeData,
+    private readonly validator: Validator
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validator.validate(httpRequest)
       const fakeDataModel = await this.addFakeData.add(httpRequest.body)
       return ok(fakeDataModel)
     } catch (error) {
