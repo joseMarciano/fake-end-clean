61887038a0753e16621a3f2deca73eafd6397475
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-16 12:58:27 -0300

    refactor: remove request validation of AddFakeDataController

--- src/presentation/controllers/fakeData/add/AddFakeDataController.ts
+++ src/presentation/controllers/fakeData/add/AddFakeDataController.ts
@@ -1,19 +1,14 @@
-import { badRequest, ok, serverError } from '../../../../presentation/helper/httpHelper'
+import { ok, serverError } from '../../../../presentation/helper/httpHelper'
 import { AddFakeData } from '../../../../domain/usecases/fakeData/add/AddFakeData'
-import { Controller, HttpRequest, HttpResponse, Validator } from '../../../../presentation/protocols'
+import { Controller, HttpRequest, HttpResponse } from '../../../../presentation/protocols'
 
 export class AddFakeDataController implements Controller {
   constructor (
-    private readonly addFakeData: AddFakeData,
-    private readonly validator: Validator
+    private readonly addFakeData: AddFakeData
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const error = this.validator.validate(httpRequest)
-
-      if (error) return badRequest(error)
-
       const fakeDataModel = await this.addFakeData.add(httpRequest.body)
       return ok(fakeDataModel)
     } catch (error) {
