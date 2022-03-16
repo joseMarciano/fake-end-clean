9494600764169c3a01c1ebc5625d931aedca21c2
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-16 17:37:45 -0300

    feat: insert editFakeData route

--- src/main/routes/fakeData/fakeDataRoute.ts
+++ src/main/routes/fakeData/fakeDataRoute.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
+import { makeEditFakeDataController } from '../../../main/factories/controller/fakeData/edit/editFakeDataControllerFactory'
 import { controllerAdapter } from '../../adapters/controllers/controllerAdapter'
 import { makeAddFakeDataController } from '../../factories/controller/fakeData/add/addFakeDataControllerFactory'
 
 export default Router()
   .post(/^.+\/create$/, controllerAdapter(makeAddFakeDataController()))
+  .put(/^.+\/edit$/, controllerAdapter(makeEditFakeDataController()))
