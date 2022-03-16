8db4cad593f2a22fdf733f1c8f48f3d71d5630ed
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-16 18:04:48 -0300

    feat: insert list-all FakeData

--- src/main/routes/fakeData/fakeDataRoute.ts
+++ src/main/routes/fakeData/fakeDataRoute.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { makeFindAllFakeDataController } from '../../../main/factories/controller/fakeData/findAll/findAllFakeDataControllerFactory'
 import { makeEditFakeDataController } from '../../../main/factories/controller/fakeData/edit/editFakeDataControllerFactory'
 import { controllerAdapter } from '../../adapters/controllers/controllerAdapter'
 import { makeAddFakeDataController } from '../../factories/controller/fakeData/add/addFakeDataControllerFactory'
@@ -6,3 +7,4 @@ import { makeAddFakeDataController } from '../../factories/controller/fakeData/a
 export default Router()
   .post(/^.+\/create$/, controllerAdapter(makeAddFakeDataController()))
   .put(/^.+\/edit$/, controllerAdapter(makeEditFakeDataController()))
+  .get(/^.+\/list-all$/, controllerAdapter(makeFindAllFakeDataController()))
