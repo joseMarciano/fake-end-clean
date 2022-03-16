b5381193aa77294b7943b3f9fbdb82723b13a9b0
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-16 20:48:19 -0300

    feat: insert find-by-id FakeData route

--- src/main/routes/fakeData/fakeDataRoute.ts
+++ src/main/routes/fakeData/fakeDataRoute.ts
@@ -4,9 +4,11 @@ import { makeEditFakeDataController } from '../../../main/factories/controller/f
 import { controllerAdapter } from '../../adapters/controllers/controllerAdapter'
 import { makeAddFakeDataController } from '../../factories/controller/fakeData/add/addFakeDataControllerFactory'
 import { makePageFakeDataController } from '../../../main/factories/controller/fakeData/page/pageFakeDataControllerFactory'
+import { makeFindFakeDataByIdController } from '../../../main/factories/controller/fakeData/findById/findFakeDataByIdControllerFactory'
 
 export default Router()
   .post(/^.+\/create$/, controllerAdapter(makeAddFakeDataController()))
   .put(/^.+\/edit$/, controllerAdapter(makeEditFakeDataController()))
   .get(/^.+\/list-all$/, controllerAdapter(makeFindAllFakeDataController()))
   .get(/^.+\/page$/, controllerAdapter(makePageFakeDataController()))
+  .get('*/find-by-id/:id', controllerAdapter(makeFindFakeDataByIdController()))
