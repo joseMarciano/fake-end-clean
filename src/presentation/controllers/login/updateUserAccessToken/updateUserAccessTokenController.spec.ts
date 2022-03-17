6dc91845c5db3695ad5f062f2075beb5ba695820
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-17 15:35:05 -0300

    test: ensure UpdateUserAccessTokenController returns 401 if DbUpdateUserAccessToken returns an Error

--- src/presentation/controllers/login/updateUserAccessToken/updateUserAccessTokenController.spec.ts
+++ src/presentation/controllers/login/updateUserAccessToken/updateUserAccessTokenController.spec.ts
@@ -1,7 +1,8 @@
-import { serverError, ok } from '../../../../presentation/helper/httpHelper'
+import { serverError, ok, unauthorized } from '../../../../presentation/helper/httpHelper'
 import { UpdateUserAccessToken } from '../../../../domain/usecases/user/authentication/UpdateUserAccessToken'
 import { HttpRequest } from '../../../../presentation/protocols'
 import { UpdateUserAccessTokenController } from './UpdateUserAccessTokenController'
+import { UpdateAccessTokenError } from '../../../../domain/usecases/user/validations/UpdateAccessTokenError'
 
 const makeFakeHttpRequest = (): HttpRequest => ({
   body: {
@@ -46,6 +47,13 @@ describe('UpdateUserAccessTokenController', () => {
     jest.spyOn(dbUpdateUserAccessTokenStub, 'updateUserAccessToken').mockImplementationOnce(() => { throw new Error() })
     const httpResponse = await sut.handle(makeFakeHttpRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 401 if DbUpdateUserAccessToken returns UpdateUserAccessError', async () => {
+    const { sut, dbUpdateUserAccessTokenStub } = makeSut()
+    jest.spyOn(dbUpdateUserAccessTokenStub, 'updateUserAccessToken').mockResolvedValueOnce(new UpdateAccessTokenError())
+    const httpResponse = await sut.handle(makeFakeHttpRequest())
+    expect(httpResponse).toEqual(unauthorized(new UpdateAccessTokenError()))
   })
 
   test('Should return 200 if DbUpdateUserAccessToken succeeds', async () => {
