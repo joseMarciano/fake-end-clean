af31da473b8a3ba884bd9939b4b909a8d99e69b8
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-17 18:12:42 -0300

    feat: insert updateAccessToken in route

--- src/main/routes/login/loginRoute.ts
+++ src/main/routes/login/loginRoute.ts
@@ -4,11 +4,13 @@ import { makeLoginController } from '../../../main/factories/controller/login/lo
 import { controllerAdapter } from '../../adapters/controllers/controllerAdapter'
 import { makeActiveSignUpController } from '../../factories/controller/login/active/activeSignUpControllerFactory'
 import { makeSignUpController } from '../../factories/controller/login/signup/signUpControllerFactory'
+import { makeUpdateAccessTokenController } from '../../../main/factories/controller/login/updateAccessToken/updateAccessTokenControllerFactory'
 
 export const loginFreeRoute = Router()
   .post('/signup', controllerAdapter(makeSignUpController()))
   .get('/active', controllerAdapter(makeActiveSignUpController()))
   .post('/login', controllerAdapter(makeLoginController()))
+  .post('/access-token', controllerAdapter(makeUpdateAccessTokenController()))
 
 export const loginAuthRoute = Router()
   .post('/', controllerAdapter(makeLogOutController()))
